012feff5d9179027bd6ef87890e2bb72344f0483
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2020-05-30 18:03:57 -0500

    First view created.

--- App.js
+++ App.js
@@ -1,19 +1,103 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends Component {
+    greet = () => {
+        Alert.alert('Es un gusto tenerte aqui.');
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <View style={styles.headerLeft}>
+                        <Image source={ require('./assets/welcome.png') } style={styles.logo} />
+                    </View>
+
+                    <View style={styles.headerRight}>
+                        <Button title="Saludo" onPress={ this.greet } />
+                    </View>
+                </View>
+
+                <View style={ [styles.body, styles.fonts] }>
+                    <Text>¡¡¡Mi first movil app.!!!</Text>
+                </View>
+
+                <View style={ [styles.footer, styles.fonts] }>
+                    <View style={styles.footerLeft}>
+                        <Text>@User</Text>
+                    </View>
+
+                    <View style={ [styles.footerCenter, styles.fonts]}>
+                        <Text>¡Saludos!</Text>
+                    </View>
+
+                    <View style={ [styles.footerRight, styles.fonts]}>
+                        <Image source={ require('./assets/users.png') } style={styles.logo} />
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: 'column'
+    },
+
+    header: {
+        flex: 0.3,
+        flexDirection: 'row',
+        marginTop: 40
+    },
+
+    headerLeft: {
+        flex: 1,
+    },
+
+    headerRight: {
+        flex: 1,
+        marginRight: 10
+    },
+
+    fonts: {
+        fontWeight: 'bold',
+    },
+
+    body: {
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        resizeMode: 'contain'
+    },
+
+    footer: {
+        flex: 0.3,
+        flexDirection: 'row',
+        marginBottom: 40,
+        alignItems: 'center',
+    },
+
+    footerLeft: {
+        flex: 1,
+        marginLeft: 25
+    },
+
+    footerCenter: {
+        flex: 1,
+    },
+
+    footerRight: {
+        flex: 1,
+    },
 });
+
+export default App;
