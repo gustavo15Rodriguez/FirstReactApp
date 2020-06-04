e29a58a16ae812debc9e4127a99b88b6da3bfcd3
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2020-06-04 15:52:16 -0500

    Using 'Push notifications' from expo.

--- src/modules/login/containers/Login.js
+++ src/modules/login/containers/Login.js
@@ -2,8 +2,35 @@ import React, { Component } from "react";
 import { Container, Content, Card, CardItem, Text, Body, Button, Item, Input } from "native-base";
 import {StyleSheet} from "react-native";
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import {Notifications} from "expo";
 
 class Login extends Component {
+    state = {
+        expoPushToken: '',
+        notification: {},
+    };
+
+    async componentDidMount() {
+        if (Constants.isDevice) {
+            const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            let token = await Notifications.getExpoPushTokenAsync();
+            console.log(token);
+            this.setState({ expoPushToken: token });
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+    }
     register = () => {
         this.props.navigation.navigate('Register');
     }
